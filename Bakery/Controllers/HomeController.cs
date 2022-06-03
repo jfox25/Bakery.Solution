cc8ff256906e213d89cfb25e5d0b8b56aa3aca51
James Fox <jimmyfox00@gmail.com>
2022-06-03 08:38:41 -0700

    Add basic project file structure

---
+++ Bakery/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+namespace Bakery.Controllers
+{
+  public class HomeController : Controller
+  {
+     [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+  }
+}
