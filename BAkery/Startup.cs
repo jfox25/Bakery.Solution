7b74dfdf0e95750a637511c075c2821a9ceb11b3
James Fox <jimmyfox00@gmail.com>
2022-06-03 17:06:57 -0700

    Startup Fix

--- BAkery/Startup.cs
+++ BAkery/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Bakery.Models;
 using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace Bakery
 {
